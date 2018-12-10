dd637cfcba5028632f10d3767216f0d68fbaa67e
josh-sea <joshua.cocciardi@gmail.com>
2018-12-10 13:56:52 -0500

    Done.

--- spec/sinatra_basic_views_lab_spec.rb
+++ spec/sinatra_basic_views_lab_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 describe 'App' do
-  
+
   describe "GET '/'" do
     before do
       get '/'
     end
-    
+
     it "returns a 200 status code" do
       expect(last_response.status).to eq(200)
     end
-
 
     it "renders index.erb" do
       expect(last_response.body).to eq(File.read("views/index.erb"))
